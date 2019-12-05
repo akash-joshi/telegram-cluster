c0659f4fabec7704b540cabde532ddd4170c91bd
Unknown <jakash1997@gmail.com>
2019-12-05 12:38:40 +0530

    Last Update ?

--- news.py
+++ news.py
@@ -4,19 +4,22 @@ start_time = time.time()
 
 news_array = []
 
+
 def detect_en_news(article):
-  print(article)
-  with open(article["path"]) as f:
-    if 'news' in f or 'News' in f:
-      print('here')
-      news_array.append(article)
+    print(article)
+    with open(article["path"]) as f:
+        content = f.read()
+        if 'news' in content or 'News' in content:
+            news_array.append(article["file"])
+
 
 def detect_news(articles):
-  if articles["lang_code"] == "en" :
-    list(map(detect_en_news, articles["articles"]))
-  return None
+    if articles["lang_code"] == "en":
+        list(map(detect_en_news, articles["articles"]))
+    return None
+
 
 def main(articles, mypath):
-  list(map(detect_news, articles))
-  print(news_array)
-  return None
+    list(map(detect_news, articles))
+    print({"articles": news_array})
+    return None
---
+++ test.py
@@ -0,0 +1,2 @@
+with open('./00-full/1003909010921209797.html') as f:
+  print(f.read())
